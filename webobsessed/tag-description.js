83adee2afb2d7c1748c34a6ab68eb89c0fa8f9f8
liza-sem <168638728+liza-sem@users.noreply.github.com>
2024-07-01 00:13:19 +0100

    Update tag-description.js

--- webobsessed/tag-description.js
+++ webobsessed/tag-description.js
@@ -151,30 +151,36 @@ $(document).ready(function() {
     };
 
 
-function insertTagDescriptions() {
+
+    function insertTagDescriptions() {
         $('.tag-description').remove();
         const activeLinks = $('.archive-group-name-link.active');
+
         activeLinks.each(function() {
             const filterValue = $(this).attr('data-filter-val');
+
             if (filterValue) {
                 let { name, description, link } = tagDescriptions[filterValue] || {};
+
                 if (!name) {
                     name = $(this).text().trim();
                 }
+
                 if (!name) {
                     return;
                 }
+
                 const newDiv = `
                     <div class="tag-description" df-filter-value="${filterValue}">
                         <a href="${link || '#'}" class="cf-df-name" target="_blank">${name}</a>
                         <div class="cf-df-description">${description || ''}</div>
                     </div>
                 `;
+
                 $('.content .blog-basic-grid').prepend(newDiv);
             }
         });
     }
-
 
     const observer = new MutationObserver(function(mutationsList, observer) {
         let relevantChangeDetected = false;
