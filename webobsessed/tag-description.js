eb4ee5408bf31a977b379951fb2b1a1096ab16d9
liza-sem <168638728+liza-sem@users.noreply.github.com>
2024-07-13 21:00:27 +0100

    Update tag-description.js

--- webobsessed/tag-description.js
+++ webobsessed/tag-description.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
     const tagDescriptions = {
         //i want to insert the string here
-  
 "kirby": {
     "name": "Kirby",
     "description": "Kirby is a file-based CMS for websites, apps, and data-driven products.",
     "link": "https://getkirby.com/?ref=digif33ls.com"
 },
-
-
-
       "astro": {
     "name": "Astro",
     "description": "Astro is a JavaScript web framework optimized for building fast, content-driven websites by rendering components on the server and delivering lightweight HTML to the browser. ",
