a76ff1d8615c448f0337c399c62070f145055f06
liza-sem <168638728+liza-sem@users.noreply.github.com>
2024-07-14 14:00:19 +0100

    Update file with new lines and remove edited lines from Google Sheets

--- webobsessed/tag-description.js
+++ webobsessed/tag-description.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     const tagDescriptions = {
         //i want to insert the string here
 
-"san-francisco": {
+"
+"rhymes": {
+    "name": "Rhymes",
+    "description": "By Jakub Samek",
+    "link": "https://maxitype.com/typeface/rhymes/?ref=digif33ls.com"
+},
+san-francisco": {
     "name": "San Francisco",
     "description": "By Apple",
     "link": "https://developer.apple.com/fonts/?ref=digif33ls.com"
