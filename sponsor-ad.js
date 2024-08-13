f0f5f92e2b00f6a1d83d1d4a179c98f4af685bd6
liza-sem <168638728+liza-sem@users.noreply.github.com>
2024-08-13 17:27:03 +0100

    Update sponsor-ad.js

--- sponsor-ad.js
+++ sponsor-ad.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const sponsoredAdHTML = `<article class="blog-basic-grid--container entry blog-item custom-filter-grid-item tag category category style features platform font resource-type tool-type element-showed is-loaded sponsored-ad" data-index="" data-publish-on=null data-categories_sort="null" style="transition: all 0.3s ease-in-out;" data-show="true" data-loaded="true">
-    <a href="squarespace.com" target="_blank">
+    <a href="https://squarespace.com" target="_blank">
       <div>
         <span class="image-wrapper preFade fadeIn" data-animation-role="image" style="transition-timing-function: ease; transition-duration: 0.4s; transition-delay: 0.171429s;">
           <img data-src="https://images.squarespace-cdn.com/content/v1/6516da875d4cda040bcc69c4/f80d0e15-c6f2-4521-a10e-2833134b1ebb/Rutgers+Practicum%2C+Spring+2022.jpeg?format=2500w" data-image-focal-point="0.5,0.5" alt="sqaurespace" data-load="false" src="https://images.squarespace-cdn.com/content/v1/6516da875d4cda040bcc69c4/f80d0e15-c6f2-4521-a10e-2833134b1ebb/Rutgers+Practicum%2C+Spring+2022.jpeg?format=2500w" class="image loaded" style="display: block; position: absolute; height: 100%; width: 100%; object-fit: cover; object-position: 50% 50%; opacity: 1;" loading="lazy" decoding="async" data-loader="sqs">
